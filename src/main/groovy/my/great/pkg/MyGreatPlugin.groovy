a82901027d4ccd427bd3a755b62920af6a03ce9e
Sergiy Beley <Sergiy.Beley@gmail.com>
2016-04-20 20:14:12 +0300

    Add sample plugin class and registred that plugin

---
+++ src/main/groovy/my/great/pkg/MyGreatPlugin.groovy
@@ -0,0 +1,13 @@
+package my.great.pkg
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class MyGreatPlugin implements Plugin<Project> {
+
+	@Override
+	public void apply(Project prj) {
+		// TODO Auto-generated method stub
+	}
+
+}
