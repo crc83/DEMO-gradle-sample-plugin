4cce7209df0dc4fa248e1ddef0f57452da492d2a
Sergiy Beley <Sergiy.Beley@gmail.com>
2016-04-20 20:29:03 +0300

    speck for sample plugin (currently it fails)

---
+++ src/test/groovy/my/great/pkg/MySuperPluginSpec.groovy
@@ -0,0 +1,67 @@
+package my.great.pkg
+
+import org.gradle.testkit.runner.GradleRunner
+import static org.gradle.testkit.runner.TaskOutcome.*
+
+class MySuperPluginSpec extends BaseSpeck {
+
+	def setup() {
+		buildFile << """
+                     plugins {
+                           id 'java'
+                           id 'my-super-plugin'
+                     }
+
+                     version = '0.1.0'
+                     group = 'foo.bar'
+              """ .stripIndent ()
+
+		settingsFile << """rootProject.name = 'foo-bar'""" .stripIndent ()
+	}
+
+	private void createHelloWorld() {
+		def src = new File(testProjectDir .folder , 'src/main/java/example')
+		src.mkdirs()
+
+		new File(src, 'HelloWorld.java').text = '''\
+              package example;
+
+              /**
+			   * HelloWorld class for test
+			   *
+			   * @copyright.placeholder@
+			   */
+              public class HelloWorld {
+              }
+              ''' .stripIndent ()
+
+		def resources = new File(testProjectDir. folder, 'src/main/resources' )
+		resources.mkdirs()
+
+		new File(resources, 'foobar.properties').text = '''\
+              # some comments
+              * @copyright.placeholder@
+              useful text
+              ''' .stripIndent ()
+	}
+
+	def 'sources jar is created'() {
+		given:
+			createHelloWorld();
+
+		when:
+			def result
+			result = GradleRunner.create()
+								.withProjectDir( testProjectDir.root)
+								.withArguments( 'sourcesJar')
+								.withPluginClasspath( pluginClasspath)
+								.forwardOutput()
+								.build() //
+
+		 then:
+		 	result.task( ":sourcesJar").outcome == SUCCESS //import static org.gradle.testkit.runner.TaskOutcome.*
+			File resultJar= new File(testProjectDir .root, 'build/libs/test-0.1.0-sources.jar' )
+			resultJar.exists()
+	}
+
+}
